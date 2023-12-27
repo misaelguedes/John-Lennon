087729a8f7ca14956d38c9c11773d04dd4312696
Misael Guedes de Lima <124177593+misaelguedes@users.noreply.github.com>
2023-12-27 12:45:33 -0300

    Criando a página Home

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Header from './components/header'
+import Home from './pages/home'
 
 export default function RoutesApp() {
     return (
         <BrowserRouter>
             <Header/>
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+            </Routes>
         </BrowserRouter>
     )
 }
