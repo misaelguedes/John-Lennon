22032a4016396146148b885b123fa11cef129c57
Misael Guedes de Lima <124177593+misaelguedes@users.noreply.github.com>
2023-12-27 15:21:23 -0300

    Criando a página History e os dois primeiros títulos da biografia

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Header from './components/header'
 import Home from './pages/home'
+import History from './pages/history'
 import Footer from './components/footer'
 
 export default function RoutesApp() {
@@ -9,6 +10,7 @@ export default function RoutesApp() {
             <Header/>
             <Routes>
                 <Route path='/' element={<Home/>}/>
+                <Route path='/historia' element={<History/>}/>
             </Routes>
             <Footer/>
         </BrowserRouter>
