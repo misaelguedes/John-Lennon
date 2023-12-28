bf9ffe9a64bd6e82b7f81ccecd972faa84e5e291
Misael Guedes de Lima <124177593+misaelguedes@users.noreply.github.com>
2023-12-28 15:38:09 -0300

    Criando media query do Header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,17 +1,48 @@
 import './header.css'
 import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 
 import JohnLennon from '../../assets/john-lennon.jpg'
 
 export default function Header() {
+
+    const [itensVisible, setItensVisible] = useState(false)
+
+    useEffect(() => {
+        function mudouTamanho() {
+            setItensVisible(window.innerWidth >= 768)
+        }
+
+        window.addEventListener('resize', mudouTamanho);
+
+        mudouTamanho();
+
+        return () => {
+            window.removeEventListener('resize', mudouTamanho);
+          };
+    }, [])
+
+    function clickMenu() {
+        setItensVisible(!itensVisible)
+    }
+
+    function hideMenu() {
+        if (window.innerWidth <= 768) {
+            setItensVisible(false)
+        }
+    }
+
     return (
         <header>
             <img className='capa' src={JohnLennon} alt='John Lennon'/>
             <div className='menu'>
-                <div className='itens'>
-                    <Link to='/'>Home</Link>
-                    <Link to='/historia'>História</Link>
-                    <Link to='/discografia'>Discografia</Link>
+                <div className='itens' style={{ display: itensVisible ? 'block' : 'none' }}>
+                    <Link onClick={hideMenu} to='/'>Home</Link>
+                    <Link onClick={hideMenu} to='/historia'>História</Link>
+                    <Link onClick={hideMenu} to='/discografia'>Discografia</Link>
+                </div>
+                <div className='burguer' onClick={clickMenu}>
+                    <span>&equiv;</span>
                 </div>
             </div>
         </header>
