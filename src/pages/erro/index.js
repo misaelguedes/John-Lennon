a5d0f923c6288ae56b4864281a0ce597f515a150
Misael Guedes de Lima <124177593+misaelguedes@users.noreply.github.com>
2023-12-28 16:33:51 -0300

    Criando className para a section da página de erro

--- src/pages/erro/index.js
+++ src/pages/erro/index.js
@@ -2,7 +2,7 @@ import './erro.css'
 
 export default function Erro() {
     return (
-        <section>
+        <section className='secterror'>
             <h2 className='erro'>404</h2>
             <p className='error'>Página não encontrada!</p>
         </section>
