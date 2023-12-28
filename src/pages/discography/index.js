848558eac5a86303f0cdd68804e4f38f1e48891e
Misael Guedes de Lima <124177593+misaelguedes@users.noreply.github.com>
2023-12-28 12:47:35 -0300

    Criando a página Discography

---
+++ src/pages/discography/index.js
@@ -0,0 +1,32 @@
+import './discography.css'
+
+export default function Discography() {
+    return (
+        <section>
+            <h1>Discografia</h1>
+            <h2>Solo</h2>
+            <ul>
+                <li>John Lennon/Plastic Ono Band (Apple, 1970)</li>
+                <li>Imagine (Apple, 1971)</li>
+                <li>Mind Games (Apple, 1973)</li>
+                <li>Walls and Bridges (Apple, 1974)</li>
+                <li>Rock 'n' Roll (Apple, 1975)</li>
+            </ul>
+            <h2>Com Yoko Ono</h2>
+            <ul>
+                <li>Unfinished Music No. 1: Two Virgins (Apple, 1968)</li>
+                <li>Unfinished Music No. 2: Life with the Lions (Zapple, 1969)</li>
+                <li>Wedding Album (Apple, 1969)</li>
+                <li>Some Time in New York City (Apple, 1972)</li>
+                <li>Double Fantasy (Geffen, 1980)</li>
+                <li>Milk and Honey (Polydor, 1984)</li>
+            </ul>
+            <h1>Filmografia</h1>
+            <p>A primeira incursão do Fab Four ao cinema data de 1964, com A Hard Day's Night, dirigido por Richard Lester. Este documentário paródico em preto e branco mostra como os Beatles viviam em plena Beatlemania. No entanto, a representação da loucura que os cerca é diluída porque, na realidade, os quatro Beatles estavam começando a viver cada vez mais loucamente, especialmente Lennon. Ele gradualmente afunda-se em uma dor emocional, que ele transpõe em sua canção "Help!", ponto de partida do filme com o mesmo nome, novamente feito por Lester em 1965. Desta vez, o filme é colorido e a história é totalmente fictícia: os Beatles são perseguidos por uma seita hindu que busca recuperar um anel de sacrifício que Ringo usa no dedo. A crítica não aprovou o filme em seu lançamento.</p>
+            <p>Em agosto de 1966, a Beatlemania assumiu uma escala assustadora e perigosa, frustrando a banda. Eles decidem parar de fazer turnês e parar de tocar em público. Lennon não vê bem essa decisão que, para ele, significa o fim dos Beatles. Ele tenta encontrar outra solução e aceita um papel de soldado no filme How I Won the War, ainda produzido por Lester e lançado em 1967. Durante as filmagens, ele compôs "Strawberry Fields Forever", um dos singles dos Beatles daquele ano. Depois de Sgt. Pepper's, o grupo, sob a direção de Paul McCartney, embarca em um filme produzido pelos próprios integrantes. O resultado é Magical Mystery Tour, produzido em colaboração com Bernard Knowles e publicado no final de 1967. O filme encena os Beatles em uma viagem de ônibus psicodélica, acompanhada por uma reunião desorganizada de atores selecionados aleatoriamente. Acabou por ser um fracasso de crítica e público. As canções do filme, reunidas no EP duplo Magical Mystery Tour, seguem a mesma veia psicodélica que as de Sgt. Pepper's.</p>
+            <p>Após esse primeiro fracasso crítico e comercial e seu encontro com Yoko Ono, Lennon tenta sair dos Beatles e, no final de 1968, participa de The Rock and Roll Circus, um programa musical organizado pelos Rolling Stones. Anteriormente, ele deveria estar envolvido com o grupo no filme de animação Yellow Submarine, dirigido por George Dunning. Mas os Beatles, desinteressados no projeto, não emprestam suas vozes e se dedicam exclusivamente a fornecer um punhado de canções, coletadas posteriormente no álbum de mesmo nome</p>
+            <p>O último filme de John com os Beatles é um testemunho da separação do grupo. No início de 1969, a banda teve que fazer um último filme para respeitar seu contrato com a United Artists, embora não tivesse mais o desejo de fazer uma comédia. Por isso, decidiram filma-los em ensaio, para um concerto final no telhado dos escritórios da Apple. No entanto, as tensões são evidentes durante as filmagens e são refletidas no filme. Os Beatles esperaram um ano antes de lança-lo, pois estavam insatisfeitos com o resultado. Let It Be, dirigido por Michael Lindsay-Hogg, foi lançado em 1970, pouco antes do álbum homônimo. Quando foi lançado, o grupo já havia se separado.</p>
+            <p>Em 1968, algum tempo antes dos Beatles se separarem, Yoko Ono iniciou Lennon em curtos filmes experimentais. O casal produziu mais de trinta até 1972. A maioria consiste em trechos de concertos e clipes filmados, enquanto outros têm um conceito bem definido, por exemplo, Self-Portrait, que mostra o pênis de John na fase de ereção, e Erection, que apresenta em time lapse, a construção do International Hotel London.</p>
+        </section>
+    )
+}
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Header from './components/header'
 import Home from './pages/home'
 import History from './pages/history'
+import Discography from './pages/discography'
 import Footer from './components/footer'
 
 export default function RoutesApp() {
@@ -11,6 +12,7 @@ export default function RoutesApp() {
             <Routes>
                 <Route path='/' element={<Home/>}/>
                 <Route path='/historia' element={<History/>}/>
+                <Route path='/discografia' element={<Discography/>}/>
             </Routes>
             <Footer/>
         </BrowserRouter>
